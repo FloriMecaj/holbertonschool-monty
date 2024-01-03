7d71db1d80d711140836974fd47cb0e4d5f577f7
eneamilo91 <eneamilo514@gmail.com>
2024-01-03 11:41:07 +0000

    added 2 files

---
+++ pall.c
@@ -0,0 +1,13 @@
+void pall(stack_t **stack, unsigned int line_number)
+{
+    stack_t *virtual = *stack;
+
+    if (*stack == NULL)
+        return;
+        
+    while(virtual != NULL)
+    {
+        printf("%d", virtual->n);
+        virtual = virtual->next;
+    }
+}
---
+++ push.c
@@ -0,0 +1,20 @@
+void push(stack_t **stack, unsigned int line_number)
+{
+    stack_t *newnode = malloc(sizeof(stack_t));
+
+    if (newnode == NULL)
+        printf("Error: malloc failed\n");
+    
+    newnode->n = line_number;
+    newnode->next = NULL;
+    newnode->prev = NULL;
+    stack_t *tmp = NULL;
+
+    if (*stack == NULL)
+        *stack = newnode;
+    
+    tmp = *stack;
+    newnode->next = tmp;
+    tmp->prev = newnode;
+    *stack = newnode;
+}
