46269bdc1c83b46362bf8592cee4c5938f5f68a6
eneamilo514 <eneamilo514@gmail.com>
2024-01-05 03:04:34 -0800

    changes

--- pall.c
+++ pall.c
@@ -8,15 +8,15 @@
 
 void pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *virtual = *stack;
-    (void) line_number;
+	stack_t *virtual = *stack;
+	(void) line_number;
 
-    if (*stack == NULL)
-        return;
+	if (*stack == NULL)
+		return;
  
-    while(virtual != NULL)
-    {
-        printf("%d", virtual->n);
-        virtual = virtual->next;
-    }
+	while(virtual != NULL)
+	{
+		printf("%d", virtual->n);
+		virtual = virtual->next;
+	}
 }
--- push.c
+++ push.c
@@ -1,31 +1,32 @@
 #include "monty.h"
+
 /**
 * push -pushes a new element into the stack
 * @stack: -points to the head of the stack
 * @line_number: holds value for the line number in monty file
 */
+
 int number;
 void push(stack_t **stack, unsigned int line_number)
 {
+	stack_t *newnode = malloc(sizeof(stack_t));
 
-    stack_t *newnode = malloc(sizeof(stack_t));
-
-    if (newnode == NULL)
-    {
-        printf("Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+	if (newnode == NULL)
+	{
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
     
-    newnode->n = number;
-    newnode->next = NULL;
-    newnode->prev = NULL;
+	newnode->n = number;
+	newnode->next = NULL;
+	newnode->prev = NULL;
 
-    if (*stack == NULL)
-        *stack = newnode;
-    else
-    {
-        newnode->next = *stack;
-        (*stack)->prev = newnode;
-        *stack = newnode;
-    }
+	if (*stack == NULL)
+		*stack = newnode;
+	else
+	{
+		newnode->next = *stack;
+		(*stack)->prev = newnode;
+		*stack = newnode;
+	}
 }
